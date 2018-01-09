1f2e1a64296bd337dae59990897ce6e9cf093338
Raja Kedia(miyagix) <rajakedia2222@gmail.com>
2018-01-09 19:51:42 +0530

    Added onchange and parallelshell

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+$(document).ready(function(){
+    $("#mycarousel").carousel({interval: 2000});
+    $("#carousel-pause").click(function(){
+        $("#mycarousel").carousel('pause');
+    });
+    /*
+    $("#carousel-pause").click(function(){
+        $("#mycarousel").carousel('pause');
+    });
+    $("#carousel-play").click(function(){
+        $("#mycarousel").carousel('cycle');
+    });
+    */
+    $("#carousel-button").click(function(){
+        if($("#carousel-button").children("span").hasClass("fa-pause")){
+            $("#mycarousel").carousel('pause');
+            $("#carousel-button").children("span").removeClass("fa-pause");
+            $("#carousel-button").children("span").addClass("fa-play");
+        }else{
+            $("#mycarousel").carousel('cycle');
+            $("#carousel-button").children("span").removeClass("fa-play");
+            $("#carousel-button").children("span").addClass("fa-pause");
+        }
+
+    });
+});
